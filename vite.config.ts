8a9d40639d3947269a43174ddc6d06fc7b1ba5b2
Sweta <swetasingh8844@gmail.com>
2026-02-22 23:23:49 +0530

    done

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js',
+      }
+    }
+  }
 });
